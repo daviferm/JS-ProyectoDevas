a97183824d3c2179d21d59133d6912198117e6e3
David Fernandez <david_ferm@hotmail.com>
2018-04-13 10:57:06 +0200

    Mejoras
    Añadir nombre del barrio en el selector y aumentar tamaño del plano.

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ let ui = new UI(latlng, zoon);
 let lat, long;
 const parkimetros = [...items];
 
+//Comprobar datos del formulario y mostrarlos en pantalla
 function obtenetDatos(e) {
   e.preventDefault();
   const spinner = document.querySelector('.spinner');
@@ -37,75 +38,68 @@ function obtenetDatos(e) {
 		}else if (met.length == 3) {
 			MET.style.borderColor = '';
 			met = 0 + MET.value;
-
-			for(let i=0; i < parkimetros.length; i++){
-				if( parkimetros[i].startsWith(barrioSeleccionado, 3) && parkimetros[i].endsWith(met) ){
-					let mostrar = dataAlias[i];
-					html = `<p>NÚMERO: ${mostrar.alias}</p>
-					<p>BARRIO: ${mostrar.barrio}</p>
-					<p>DIRECCIÓN: ${mostrar.direccion}</p>
-					<p>FABRICANTE: ${mostrar.fabricante}</p>
-					<p>TARIFA: ${mostrar.tarifa}</p>
-					`;
-					botonMap = `<button type="button" class="mostrarMapa">Mapa</button>`
-					INFO.className = 'info';
-					INFO.innerHTML = html;
-					INFO.style.display = 'block';
-					document.querySelector('#mapa').style.top = '15px';
-					lat = Number( mostrar.latitud );
-					lng = Number( mostrar.longitud );
-					zoon = 17;
-					latlng = {lat: lat, lng: lng};
-					ui = new UI(latlng, zoon);
-					ui.mostrarPin(latlng);
-
-					break;
-				}else{
-					document.querySelector('#mapa').style.top = '20px';
-					INFO.style.display = 'block';
-					INFO.classList.add('noEncontrado');
-					html = `<h3>El número de Barrio</br>
-					o Parkímetro es incorrecto...</h3>`;
-					INFO.innerHTML = html;
-				}
-			}
+			mostrarInfo(barrioSeleccionado, met);
+		}else if(met.length === 4){
+			mostrarInfo(barrioSeleccionado, met);
 		}
+
 		spinner.classList.remove('animation');
-  }, 450);
+  }, 200);
 }
 
+//Mostrar el mapa en con la posición del parkímetro
 function mostrarMapa () {
 	document.getElementById('mapa').style.display = 'block';
 	document.getElementById('map').style.display = 'block';
 }
 
+//Función para mostrar la información del parkímetro
+function mostrarInfo(barrioSeleccionado, met){
+	for(let i=0; i < parkimetros.length; i++){
+		if( parkimetros[i].startsWith(barrioSeleccionado, 3) && parkimetros[i].endsWith(met) ){
+			console.log(parkimetros[i]);
+			let mostrar = dataAlias[i];
+			html = `<p>NÚMERO: ${mostrar.alias}</p>
+			<p>BARRIO: ${mostrar.barrio}</p>
+			<p>DIRECCIÓN: ${mostrar.direccion}</p>
+			<p>FABRICANTE: ${mostrar.fabricante}</p>
+			<p>TARIFA: ${mostrar.tarifa}</p>
+			`;
+			botonMap = `<button type="button" class="mostrarMapa">Mapa</button>`
+			INFO.className = 'info';
+			INFO.innerHTML = html;
+			INFO.style.display = 'block';
+			document.querySelector('#mapa').style.top = '15px';
+			lat = Number( mostrar.latitud );
+			lng = Number( mostrar.longitud );
+			zoon = 17;
+			latlng = {lat: lat, lng: lng};
+			ui = new UI(latlng, zoon);
+			ui.mostrarPin(latlng);
+
+			break;
+		}else{
+			document.querySelector('#mapa').style.top = '20px';
+			INFO.style.display = 'block';
+			INFO.classList.add('noEncontrado');
+			html = `<h3>El número de Barrio</br>
+			o Parkímetro es incorrecto...</h3>`;
+			INFO.innerHTML = html;
+		}
+	}
+}
 
 
 //Lenamos el listado de barrios
+const barriosHTML = ["44 Guindalera","45 Lista","46 Castellana","51 El Viso","52 Prosperidad","53 Ciudad Jardín","54 Hispanoamérica","55 Nueva España","56 Castilla","61 Bellas Vistas","62 Cuatro Caminos","63 Castillejos","64 Almenara","65 Valdeacederas","66 Berruguete","75 Rios Rosas","76 Vallehermoso","84 Pilar","85 La Paz","93 Ciudad Universitaria"];
 const listaBarrios = [44,45,46,51,52,53,54,55,56,61,62,63,64,65,66,75,76,84,85,93];
 
 for(let i = 0; i < listaBarrios.length; i++){
   let option = document.createElement('option');
 	option.value = listaBarrios[i];
-	option.innerHTML = listaBarrios[i];
+	option.innerHTML = barriosHTML[i];
 	selectBarrio.appendChild(option);
 }
-
-// const UL_BARRIO = document.getElementById('Barrios');
-// const LISTA = document.getElementById('Lista_barrios');
-// const MENU = document.querySelector('.lista');
-// UL_BARRIO.addEventListener("focus", deplegarMenu);
-// UL_BARRIO.addEventListener("blur", plegarMenu);
-
-
-// function deplegarMenu () {
-// 	MENU.style.height = '200px';
-// 	LISTA.style.display = 'block';
-// }
-// function plegarMenu () {
-// 	LISTA.style.display = 'none';
-// }
-
 
 
 
