857d7f63720b17c68498707f621ffc8f6ba0511c
daviferm <33403711+daviferm@users.noreply.github.com>
2018-05-07 16:30:09 +0200

    Actualizar js

--- js/listaApp.js
+++ js/listaApp.js
@@ -5,24 +5,45 @@ const MAPA_BOTON = document.getElementById('botonMapa');
 const MET = document.querySelector("input");
 
 document.addEventListener('DOMContentLoaded', mostrarFichas);
+document.addEventListener('DOMContentLoaded', localStorageload);
 
 document.querySelector('#formulario').addEventListener('submit', añadirMet);
 
+//Ciclo para activar las plantillas
 for(let i = 0; i < $plantillas.length; i++){
   $plantillas[i].addEventListener('click', activarPlantilla);
 }
 
+//Clase para añadir listas de tareas
 class Ficha {
-  constructor(num){
-    this.lista = num;
+  constructor(id, met){
+    this.lista = id;
+    this.met = met;
+    this.name = "lista_"+id;
+    this.anadirLocalStorage(met);
+  }
+  anadirLocalStorage(met){
+    
+    let items = this.obtenerDatosLocalStorage();
+    items.push(met);
+
+    localStorage.setItem(this.name, JSON.stringify(items));
+  }
+  obtenerDatosLocalStorage() {
+    let elementos;
+    let elem = this.name;
+    // Revisar valores de localstorage
+    if(localStorage.getItem( elem ) === null) {
+        elementos = [];
+        console.log(elementos);
+    } else {
+        elementos = JSON.parse(localStorage.getItem(elem) );
+    }
+    return elementos;
   }
   
 }
-const listaUno = new Ficha(uno);
-const listaDos = new Ficha(dos);
-const listaTres = new Ficha(tres);
-const listaCuatro = new Ficha(cuatro);
-const listaCinco = new Ficha(cinco);
+
 
 
 //Funciones
@@ -34,9 +55,9 @@ function mostrarFichas(flecha){
   }
   
   let margin = 0;
-  let zIndex = 0;
+  let zIndex = 1;
   $plantillas.forEach(function(el){
-    plantilla = el.parentElement;
+    plantilla = el.parentElement.parentElement;
     plantilla.style.zIndex = `${zIndex}`;
     plantilla.style.marginTop = `${margin}px`;
     margin += 80;
@@ -44,11 +65,45 @@ function mostrarFichas(flecha){
   });
 }
 
+//Cargar los datos de localStorage
+function localStorageload(){
+  
+  let listStorage = ['lista_uno', 'lista_dos', 'lista_tres', 'lista_cuatro', 'lista_cinco'];
+  listStorage.forEach(function(el){
+    if(localStorage.getItem(el) !== null){
+      let list = JSON.parse(localStorage.getItem(el));
+      list.forEach(function(item){
+        
+        let id = el.substr( el.indexOf('_') + 1 , el.length - 1);
+        let plantilla = document.getElementById(id).children[1].children[0];
+
+        crearLi(plantilla, item);
+      })
+    }
+  })
+}
+//Borrar parkímetro de localStorage
+function borrarDatoLocalStorage(met) {
+  console.log(met);
+  let storage = JSON.parse(localStorage.getItem('lista_'+targetaId));
+  storage.forEach(function(el, index){
+    if(el.alias == met.children[0].textContent){
+
+      storage.splice(index, 1);
+      console.log(el);
+    }
+  });
+
+  localStorage.setItem('lista_'+targetaId, JSON.stringify(storage));
+
+}
 
 
-let fichaActivada, targeta;
+
+let fichaActivada, targetaId;
 function activarPlantilla(el){
-  let elemento = el.target.parentElement.parentElement;
+
+  let elemento = el.target.parentElement.parentElement.parentElement;
   let flecha = el.target.parentElement.children;
 
   for(let i = 0; i < flecha.length; i++){
@@ -58,19 +113,19 @@ function activarPlantilla(el){
   if(fichaActivada !== undefined){
     mostrarFichas(flecha);
     fichaActivada = undefined;
-    targeta = undefined;
-    console.log("Tarjeta: " + targeta);
+    targetaId = undefined;
+    console.log("Tarjeta: " + targetaId);
   }else{
-    targeta = elemento.id;
-    console.log("Tarjeta: " + targeta);
+    targetaId = elemento.id;
+    console.log("Tarjeta: " + targetaId);
     elemento.style.marginTop = '0px';
-    let plantilla = elemento.getAttribute('data-key');
+    // let plantilla = elemento.getAttribute('data-key');
     let atributo;
     let margin = 650;
     for(let i = 0; i < $plantillas.length; i++){
-      atributo = $plantillas[i].parentElement.getAttribute('data-key');
-      if(atributo !== plantilla){
-        $plantillas[i].parentElement.style.marginTop = `${margin}px`;
+      atributo = $plantillas[i].parentElement.parentElement.getAttribute('data-key');
+      if(atributo !== targetaId){
+        $plantillas[i].parentElement.parentElement.style.marginTop = `${margin}px`;
         margin += 2;
       } 
     }
@@ -111,6 +166,7 @@ function añadirMet(e){
 		}else if (met.length == 3) {
 			MET.style.borderColor = '';
       met = 0 + MET.value;
+      
       anadirInfo(barrioSeleccionado, met);
 		}else if(met.length === 4){
       anadirInfo(barrioSeleccionado, met);
@@ -121,19 +177,87 @@ function añadirMet(e){
 
 //Añadir parkímetro a la lista seleccionada
 function anadirInfo(barrio, met){
-  if(targeta !== undefined){
+  
+  if(targetaId !== undefined){
 
-    let elem = baseDatos.find(function(el){
-      return el.alias.startsWith(barrio, 3) && el.alias.endsWith(met)
+    let elem = baseDatos.findIndex(function(el){
+      
+      return el.alias.startsWith(barrio, 3) && el.alias.endsWith(met);
+      //Devuelve el index del objeto en la variable "elem"
     })
-    if(elem !== undefined){
-      console.log("Plantilla: " + targeta);
-      console.log("Parkímetro: " + elem.alias);
+
+    if(elem !== -1){
+
+      const parkimetro = baseDatos[elem];
+      const boleano = comprobarAlias(parkimetro);
+      
+      if(boleano !== true){
+        
+        const plantilla = new Ficha(targetaId, parkimetro);
+        const resultado = document.getElementById(targetaId).children[1].children[0];
+        
+        crearLi(resultado, parkimetro);
+      }else{
+        alert("El parkímetro ya está en la lista");
+      }
+
     } else {
-      console.error(elem);
+      alert("El parkímetro no se encuentra en la base de datos!");
+      console.error("El parkímetro no está en la base de datos!");
     }
-    
   } 
+}
+//Añadir li a la plantilla
+function crearLi(plantilla, parkimetro){
+  
+  let html = `
+  <label  id="alias" for"checkBox">${parkimetro.alias}</label>
+  <input id="checkBox" type="checkbox">
+  <span class="equix">X</span>
+  `;
+  let li = document.createElement('li');
+  li.classList.add('liMet');
+  li.innerHTML = html;
+  plantilla.appendChild(li);
+
+  li.addEventListener('click', mostrarInformacion);
+}
+
+//Mostrar información del parkímetro seleccionado
+function mostrarInformacion(e){
+  if(e.target.id == 'alias'){
+    const num = e.target;
+    console.log( num.parentElement);
+  }
+  if(e.target.textContent === 'X'){
+    const li = e.target.parentElement.parentElement.parentElement;
+
+    if(targetaId !== undefined){
+      
+      li.remove();
+      // console.log(e.currentTarget);
+      borrarDatoLocalStorage(li);
+    }
+
+  }
+}
+
+//Comprobar si el parkímetro ha sido añadido
+function comprobarAlias(el){
+  console.log(el);
+  // const resultado = document.getElementById(targetaId).children[1].children[0];
+  const resultado = JSON.parse(localStorage.getItem('lista_'+targetaId));
+  
+  // console.log(resultado);
+  if(resultado !== null){
+
+    for( let i = 0; i < resultado.length; i++){
+      if(el.alias == resultado[i].alias){
+        return true;
+        break;
+      }
+    }
+  }
 }
 
 const selectBarrio = document.getElementById('barriosOption');
