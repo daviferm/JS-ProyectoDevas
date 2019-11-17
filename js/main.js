7cfa974ac941a8f3da303e0eb5b9451323fcd511
David <david_ferm@hotmail.com>
2019-11-17 19:03:48 +0100

    Añadir barrio 92

--- js/main.js
+++ js/main.js
@@ -38,6 +38,8 @@ function obtenetDatos(e) {
         const barrioSeleccionado = selectBarrio.options[selectBarrio.selectedIndex].value;
         let met = MET.value;
         let html;
+        console.log(barrioSeleccionado);
+        console.log(met);
 
         if (met.length < 3 || met.length > 4) {
             MET.style.borderColor = 'red';
@@ -57,20 +59,21 @@ function obtenetDatos(e) {
 }
 
 //Función para mostrar la información del parkímetro
-  async function mostrarInfo(barrioSeleccionado, met) {
+async function mostrarInfo(barrioSeleccionado, met) {
 
-   // let data = await fetch('https://app-utedevas.firebaseio.com/parkimetros.json')
-    //    .then(async function(res) {
-    //        let respuesta = await res.json();
-     //       console.log(respuesta);
-      //      $nuevaWeb.classList.add('esconder');
-      //      return respuesta;
-      //  });
+    // let data = await fetch('https://app-utedevas.firebaseio.com/parkimetros.json')
+    //     .then(async function(res) {
+    //         let respuesta = await res.json();
+    //         console.log(respuesta);
+    //         $nuevaWeb.classList.add('esconder');
+    //         return respuesta;
+    //     });
 
-     let data = await fetch('./data/data.json')
+    let data = await fetch('./data/data.json')
         .then(async function(res) {
-             let respuesta = await res.json();
-             return respuesta;
+            let respuesta = await res.json();
+            $nuevaWeb.classList.add('esconder');
+            return respuesta.parkimetros;
         })
 
     let baseDatosMets = data;
@@ -183,8 +186,15 @@ function navegacion() {
 
 
 //Lenamos el listado de barrios
-const barriosHTML = ["44 Guindalera", "45 Lista", "46 Castellana", "51 El Viso", "52 Prosperidad", "53 Ciudad Jardín", "54 Hispanoamérica", "55 Nueva España", "56 Castilla", "61 Bellas Vistas", "62 Cuatro Caminos", "63 Castillejos", "64 Almenara", "65 Valdeacederas", "66 Berruguete", "75 Rios Rosas", "76 Vallehermoso", "84 Pilar", "85 La Paz", "93 Ciudad Universitaria"];
-const listaBarrios = [44, 45, 46, 51, 52, 53, 54, 55, 56, 61, 62, 63, 64, 65, 66, 75, 76, 84, 85, 93];
+const barriosHTML = ["44 Guindalera", "45 Lista", "46 Castellana", "51 El Viso",
+    "52 Prosperidad", "53 Ciudad Jardín", "54 Hispanoamérica",
+    "55 Nueva España", "56 Castilla", "61 Bellas Vistas",
+    "62 Cuatro Caminos", "63 Castillejos", "64 Almenara",
+    "65 Valdeacederas", "66 Berruguete", "75 Rios Rosas",
+    "76 Vallehermoso", "84 Pilar", "85 La Paz",
+    "93 Ciudad Universitaria", "94 Valdezarza"
+];
+const listaBarrios = [44, 45, 46, 51, 52, 53, 54, 55, 56, 61, 62, 63, 64, 65, 66, 75, 76, 84, 85, 93, 94];
 
 for (let i = 0; i < listaBarrios.length; i++) {
     let option = document.createElement('option');
